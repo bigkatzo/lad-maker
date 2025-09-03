05e871d8a4fd96ba17b56eb98ba637a86c50d7c8
Arik Katz <arik@Ariks-MacBook-Air-2.local>
2025-09-03 09:05:27 +0300

    Update X community link and remove Telegram link

--- src/App.tsx
+++ src/App.tsx
@@ -253,7 +253,7 @@ function App() {
           <div className="flex justify-center items-center space-x-8 flex-wrap gap-4">
             {/* X (Twitter) */}
             <a
-              href="https://x.com/i/communities/1962062999809843209"
+              href="https://x.com/i/communities/1962817192149340493"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-black hover:bg-gray-900 text-white p-4 rounded-full border-4 border-gray-800 transform hover:scale-110 transition-all duration-200 shadow-lg"
@@ -269,29 +269,6 @@ function App() {
               >
                 <path
                   d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
-                  fill="currentColor"
-                />
-              </svg>
-            </a>
-
-            {/* Telegram */}
-            <a
-              href="https://t.me/ladcoinctogroup"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-blue-500 hover:bg-blue-600 text-white p-4 rounded-full border-4 border-gray-800 transform hover:scale-110 transition-all duration-200 shadow-lg"
-              title="Join our Telegram"
-            >
-              <svg
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="text-white"
-              >
-                <path
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69a.2.2 0 00-.05-.18c-.06-.05-.14-.03-.21-.02-.09.02-1.49.95-4.22 2.79-.4.27-.76.41-1.08.4-.36-.01-1.04-.2-1.55-.37-.63-.2-1.12-.31-1.08-.66.02-.18.27-.36.75-.55 2.94-1.29 4.91-2.14 5.91-2.55 2.81-1.18 3.39-1.39 3.77-1.39.08 0 .27.02.39.12.1.08.13.19.14.27-.01.06.01.24 0 .38z"
                   fill="currentColor"
                 />
               </svg>
